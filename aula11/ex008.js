192850e711a70353bb7606efc382333d4aeeba8e
delvecch1o <rodrigopereirapinheiro@gmail.com>
2021-05-19 18:23:39 -0300

    condições01

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 61
+console.log(`A velocidade do seu carro é ${velocidade} Km/h`)
+if(velocidade > 60){ // condição simples!
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando conto de segurança`)
