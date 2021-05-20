9a0c98d4d171ca1fbb51d2f63671eec4a508556f
delvecch1o <rodrigopereirapinheiro@gmail.com>
2021-05-19 21:33:46 -0300

    horas do dia

--- ex014/script.js
+++ ex014/script.js
@@ -1,1 +1,23 @@
-alert(`Salve!`)
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 18
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        // BOM DIA
+        img.src = '../ex014/imagens/bomdia.png'
+        document.body.style.background = '#e2cd9f'
+
+    } else if(hora >= 12 && hora <= 18){
+        // BOA TARDE
+        img.src = '../ex014/imagens/boatarde.png'
+        document.body.style.background = '#b9846f'
+
+    } else{
+        // BOA NOITE
+        img.src = '../ex014/imagens/boanoite.png'
+        document.body.style.background = '#515154'
+    }
+}
