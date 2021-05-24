ba0b832526eb5d261bf527fa70de015ffffc3166
delvecch1o <rodrigopereirapinheiro@gmail.com>
2021-05-24 20:58:00 -0300

    laços de repetição

---
+++ aula13/teste.js
@@ -0,0 +1,16 @@
+/*
+var c = 1
+do  {
+console.log(`Ola que tales ${c}`)
+c++
+} while (c < 6)
+
+*/
+
+// --------------
+
+console.log(`Vai começar...`)
+for(var c = 1; c <= 5; c++ ){
+console.log(`Numero ${c}`)
+}
+console.log(`FIM !!!`)
